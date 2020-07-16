556bd3773be64b4f08671479a0402aac9e2a180e
Veronica Santos <verosbraga@gmail.com>
2020-07-16 20:08:58 +0100

    Created Main and Input classes. Developed demandInput and validateInput methods for Input class.

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -9,7 +9,35 @@ public class Input {
 
         String response = input.nextLine();  // Read user input
         String directions = response.toUpperCase().trim();
-        System.out.println("The direction(s) chosen is(are): " + directions);
+        validateInput(directions);
+        //System.out.println("The direction(s) chosen is(are): " + directions);
+        input.close();
         return directions;
+    }
+
+    public void validateInput(String answer) {
+        //to-do verification with Regex
+        /*boolean match = answer.matches("[NSEW]");
+        System.out.println(match);
+
+        if (!match) {
+            System.out.println("This input is not valid. Please insert correct options.");
+        }*/
+        if (!answer.isEmpty()) {
+            char[] cardinalPoints = answer.toCharArray();
+            //System.out.println(cardinalPoints);
+
+            for (char character : cardinalPoints) {
+                if (character != 'N' && character != 'S' && character != 'E' && character != 'W') {
+                    System.out.println("This input is not valid. Please insert correct options and without spaces. \n");
+                    demandInput();
+                    break;
+                }
+            }
+            return;
+        }
+        System.out.println("Empty answer. Please, choose one or more directions. \n");
+        demandInput();
+
     }
 }
