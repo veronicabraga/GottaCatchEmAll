fc27e491d5cfe2d3fe9b1d308afbb8a26d57c752
Veronica Santos <verosbraga@gmail.com>
2020-07-16 23:20:04 +0100

    Added while loop and changed validateInput return

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
         Input input = new Input();
-        input.demandInput();
+        String directions = input.demandInput();
+        System.out.println("The chosen direction(s) is(are): " + directions);
     }
 }
