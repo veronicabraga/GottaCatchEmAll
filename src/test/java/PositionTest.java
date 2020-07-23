c5250b11892dcef357bf853737bd3c7d065b7643
Veronica Santos <verosbraga@gmail.com>
2020-07-23 18:51:13 +0100

    Developed moveInDirection tests for all directions

--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -1,7 +1,9 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class PositionTest {
 
@@ -49,12 +51,51 @@ public class PositionTest {
         assertEquals(expected, result);
     }
 
-    /*@Test
-    public void moveInDirectionTest() {
-        position.moveInDirection(Direction.N);
-        int expected = 1;
-        int result = position.getCol();
+    @Test
+    public void moveInDirectionNTest() {
+        Position positionMock = Mockito.spy(position);
+        doNothing().when(positionMock).moveN();
+        positionMock.moveInDirection(Direction.N);
 
-        assertEquals(expected, result);
-    }*/
+        verify(positionMock, times(1) ).moveN();
+        verify(positionMock, times(0) ).moveS();
+        verify(positionMock, times(0) ).moveE();
+        verify(positionMock, times(0) ).moveW();
+    }
+
+    @Test
+    public void moveInDirectionSTest() {
+        Position positionMock = Mockito.spy(position);
+        doNothing().when(positionMock).moveS();
+        positionMock.moveInDirection(Direction.S);
+
+        verify(positionMock, times(0) ).moveN();
+        verify(positionMock, times(1) ).moveS();
+        verify(positionMock, times(0) ).moveE();
+        verify(positionMock, times(0) ).moveW();
+    }
+
+    @Test
+    public void moveInDirectionETest() {
+        Position positionMock = Mockito.spy(position);
+        doNothing().when(positionMock).moveE();
+        positionMock.moveInDirection(Direction.E);
+
+        verify(positionMock, times(0) ).moveN();
+        verify(positionMock, times(0) ).moveS();
+        verify(positionMock, times(1) ).moveE();
+        verify(positionMock, times(0) ).moveW();
+    }
+
+    @Test
+    public void moveInDirectionWTest() {
+        Position positionMock = Mockito.spy(position);
+        doNothing().when(positionMock).moveW();
+        positionMock.moveInDirection(Direction.W);
+
+        verify(positionMock, times(0) ).moveN();
+        verify(positionMock, times(0) ).moveS();
+        verify(positionMock, times(0) ).moveE();
+        verify(positionMock, times(1) ).moveW();
+    }
 }
