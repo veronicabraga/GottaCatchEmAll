63031f499ae9d5e05a3328714dacf1f6e86b11f2
Veronica Santos <verosbraga@gmail.com>
2020-07-26 19:28:51 +0100

    Created Game comments

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -6,25 +6,45 @@ public class Game {
     private Ash ash;
     private int points;
 
-
+    /**
+     * Class constructor where it instantiates a {@link Grid}, an {@link Ash} and grants value to points
+     */
     public Game() {
         grid = new Grid();
         ash = new Ash();
         points = 1;
     }
 
+    /**
+     * @return points
+     */
     public int getPoints() {
         return points;
     }
 
+    /**
+     * Sets the grid
+     *
+     * @param grid the grid to set
+     */
     public void setGrid(Grid grid) {
         this.grid = grid;
     }
 
+    /**
+     * Sets the ash
+     *
+     * @param ash the ash to set
+     */
     public void setAsh(Ash ash) {
         this.ash = ash;
     }
 
+    /**
+     * Moves ash accordingly the directions parameter
+     *
+     * @param directions
+     */
     public void moveAsh(String directions) {
         char[] cardinalPoints = directions.toCharArray();
 
@@ -56,6 +76,12 @@ public class Game {
         }
     }
 
+
+    /**
+     * Adds a cell in the grid and increase points if the grid does not contain the key(coordinate)
+     *
+     * @param coordinate
+     */
     public void handleCells(int[] coordinate) {
         if (!grid.containsKey(coordinate)) {
             grid.addCell(coordinate, 0);
